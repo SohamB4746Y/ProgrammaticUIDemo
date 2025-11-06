992a11f0e3afbabe7fb943a0afad3db299f4ec5c
SohamB4746Y <sohambhattacharya023@gmail.com>
2025-11-06 13:17:48 +0530

    Initial Commit

---
+++ ProgrammaticUIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticUIDemo
+//
+//  Created by Soham Bhattacharya on 06/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
